d7978c13771212659770afe3c928f7a19b1c345c
Michael R. Shannon <mrshannon.aerospace@gmail.com>
2016-03-01 02:06:09 -0700

    Add a basic home page.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
+var httpError = require('http-error');
+var package = require('package');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  data = {
+    title: package.name,
+    version: package.version,
+    description: package.description,
+    repository: package.repository.url,
+    contributors: package.contributors,
+    signupLink: 'signup',
+    loginLink: 'login'
+  };
+
+  res.format({
+    'text/html': function(){
+      res.render('index', data);
+    },
+
+    'application/json': function(){
+      res.json(data);
+    },
+
+    'default': function() {
+      // log the request and respond with 406
+      next(httpError(406));
+    }
+  });
+
 });
 
 module.exports = router;
