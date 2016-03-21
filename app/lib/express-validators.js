a40fb3acae0a5e92f8c96a6e056639d9f74053e5
Michael R. Shannon <mrshannon.aerospace@gmail.com>
2016-03-21 16:21:48 -0600

    Make lib/express-validators.js pass linter.

--- app/lib/express-validators.js
+++ app/lib/express-validators.js
@@ -1,8 +1,8 @@
 module.exports = {
   customValidators: {
     // username validator
-    username: function (value) {
-      return !/\s/.test(value)
-    }
-  }
+    username(value) {
+      return !/\s/.test(value);
+    },
+  },
 };
